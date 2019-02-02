2d3fd9f57370c978855b69181046a09c5994d584
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-02-02 21:09:43 +0530

    Update remove_k_from_list.py

--- remove_k_from_list.py
+++ remove_k_from_list.py
@@ -1,9 +1,11 @@
 n,k = map(int,input("").split())
 a = list(map(int,input().split()))
+c = []
 for i in a:
-    if(i == k):
-        a.remove(i)
-if(len(a) == 0):
+    if(i != k):
+        c.append(i)
+        
+if(len(c) == 0):
     print("empty")
 else:
-    print(" ".join(map(str,a)))
+    print(" ".join(map(str,c)))
