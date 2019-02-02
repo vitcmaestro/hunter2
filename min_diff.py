ba78595dc2fb2704923782a7b6c372325a5fa6f5
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-02-02 19:19:30 +0530

    Update min_diff.py

--- min_diff.py
+++ min_diff.py
@@ -1,9 +1,15 @@
 n= int(input(""))
 a = list(map(int,input().split()))
 u,v = map(int,input().split())
-x = a.index(u)
-y = a.index(v)
-if(x>y):
-    print(x-y)
-else:
-    print(y-x)
+x =[index for index, value in enumerate(a) if value == u]
+y = [index for index, value in enumerate(a) if value == v]
+miner = n
+for i in range(len(x)):
+    for j in range(len(y)):
+        if(x[i]>y[j]):
+            diff = x[i]-y[j]
+        else:
+            diff = y[j]-x[i]
+        if(diff<miner):
+            miner = diff
+print(miner)
