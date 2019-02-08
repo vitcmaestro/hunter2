90c8b80f7b6339d654deb547241dd43a238b3995
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-02-08 11:09:51 +0530

    Create Common_subarray.py

---
+++ Common_subarray.py
@@ -0,0 +1,9 @@
+a = input("")
+b = input("")
+com = ""
+for i in range(len(a)):
+    for j in range(len(a),-1,-1):
+        if(a[i:j] in b):
+            if(len(a[i:j])>len(com)):
+                com = a[i:j]
+print(com)
