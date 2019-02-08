7133c1e914b6432b13a833eacdb3fb626266f02c
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-02-08 16:45:54 +0530

    Create max_len_consec_char.py

---
+++ max_len_consec_char.py
@@ -0,0 +1,15 @@
+a = input("")
+maxer = 0
+check = a[0]
+count = 1
+ans = check
+for i in range(1,len(a)):
+    if(a[i] == check):
+        count+=1
+    else:
+        if(count > maxer):
+            maxer = count
+            ans = a[i-1]
+        count = 1
+        check = a[i]
+print(maxer,ans)
