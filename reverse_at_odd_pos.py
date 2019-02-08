9ab6bba582576801474127ed690f9fabb00da0b4
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-02-08 11:02:42 +0530

    Update reverse_at_odd_pos.py

--- reverse_at_odd_pos.py
+++ reverse_at_odd_pos.py
@@ -2,7 +2,7 @@ a = list(map(str,input().split()))
 i = len(a)-1
 if(a[len(a)-1][len(a[i])-1] == '.'):
     a[i] = a[i][:len(a[i])-1]
-for i in range(len(a)):
-    if(i%2 == 0):
-        a[i] = a[i][::-1]
+for j in range(len(a)):
+    if(j%2 == 0):
+        a[j] = a[j][::-1]
 print(" ".join(map(str,a)))
