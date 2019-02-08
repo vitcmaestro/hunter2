62023e076ca4cd46734648cbf1f5a03b3f4a87d9
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-02-08 12:24:32 +0530

    Update loop_median.py

--- loop_median.py
+++ loop_median.py
@@ -1,1 +1,13 @@
-
+n = int(input(""))
+a = list(map(int,input().split()))
+mid = len(a)//2
+if(n%2 !=0):
+    print(a[mid])
+    a.remove(a[mid])
+    n-=1
+while(len(a)!= 0 ):
+    mid = len(a)//2
+    avg = (a[mid]+a[mid-1])//2
+    a.remove(a[mid])
+    a.remove(a[mid-1])
+    print(avg)
