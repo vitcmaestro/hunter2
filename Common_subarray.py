1fe9b3db1f7968ecf8130488b08ca17060aaccaa
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-02-08 11:12:33 +0530

    Update Common_subarray.py

--- Common_subarray.py
+++ Common_subarray.py
@@ -4,6 +4,6 @@ com = ""
 for i in range(len(a)):
     for j in range(len(a),-1,-1):
         if(a[i:j] in b):
-            if(len(a[i:j])>len(com)):
+            if(len(a[i:j])>=len(com)):
                 com = a[i:j]
 print(com)
