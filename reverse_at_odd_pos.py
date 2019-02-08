cefad7496800b847323f91e7ee2ef71b967e9497
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-02-08 10:57:47 +0530

    Update reverse_at_odd_pos.py

--- reverse_at_odd_pos.py
+++ reverse_at_odd_pos.py
@@ -1,4 +1,7 @@
 a = list(map(str,input().split()))
+i = len(a)-1
+if(a[len(a)-1][len(a[i])-1] == '.'):
+    a[i] = a[i][:len(a[i])-1]
 for i in range(len(a)):
     if(i%2 == 0):
         a[i] = a[i][::-1]
