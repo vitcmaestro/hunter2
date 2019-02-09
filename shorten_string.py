4bfc166d64121642dc01ec43b15118bd12ea5f47
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-02-09 09:58:17 +0530

    Create shorten_string.py

---
+++ shorten_string.py
@@ -0,0 +1,18 @@
+n = input("")
+n = n+"."
+check = n[0]
+s = ""
+count = 1
+for i in range(1,len(n)):
+    if(n[i] == check):
+        count+=1
+    else:
+        if(count == 1):
+            s = s+n[i-1]
+            check = n[i]
+        else:
+            s = s+str(count)+"*"+n[i-1]
+            count = 1
+            check = n[i]
+           
+print(s)
