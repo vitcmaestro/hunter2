ef69dd6a689959d7953538012fe271a4629c40be
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-02-09 08:42:55 +0530

    Create print_maxx_af_dup_removal.py

---
+++ print_maxx_af_dup_removal.py
@@ -0,0 +1,11 @@
+n = input("")
+dup = []
+ans = []
+for i in range(len(n)-1):
+    x = list(n)
+    if(n[i] == n[i+1] and n[i] not in dup):
+        dup.append(n[i])
+        del x[i]
+        ans.append(x)
+pri = max(ans)
+print(" ".join(map(str,x)))
